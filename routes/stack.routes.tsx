bd90dd2cec83ee4e49f94283fb2014bd393871f0
Beatriz Pastori <83585914+beatrizpastori@users.noreply.github.com>
2021-07-13 17:58:56 -0300

    Routes

---
+++ routes/stack.routes.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const stackRoutes = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+    <stackRoutes.Navigator>
+        
+    </stackRoutes.Navigator>
+)
