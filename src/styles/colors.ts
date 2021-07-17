888fcaa767e693db0f58cc0f275f9a1124b28985
brunalsousa <bluisa10@hotmail.com>
2021-07-17 03:15:22 -0300

    Estilização (pasta styles)
    Criação da página styles e criação dos arquivos colors.ts (armazena as paletas de cores que serão utilizadas no aplicativo) e fonts.ts (tipos e tamanhos das fontes)

---
+++ src/styles/colors.ts
@@ -0,0 +1,44 @@
+export default {
+
+    //rever essas cores, caso sejam úteis
+    heading: '#52665A',
+    body_dark: '#738078',
+    body_light: '#AAB2AD',
+  
+    background: '#FFFFFF',
+    shape: '#F0F0F0',
+    white: '#FFFFFF',
+    gray: '#CFCFCF',
+    // até aqui
+
+    branco: '#FFFFFF',
+    cinza_claro: '#D3D3D3',
+
+    laranja_claro: '#FFE485',
+    laranja: '#FFB457',
+    laranja_escuro: '#FF7100',
+
+    rosa_claro: '#FFBBCC',
+    rosa: '#FF69B4',
+    rosa_escuro: '#FF008D',
+
+    roxo_claro: '#EE82EE',
+    roxo: '#C86DC6',
+    roxo_escuro: '#995398',
+
+    lilas_claro: '#D5BFD8',
+    lilas: '#C8A2C8',
+    lilas_escuro: '#A283A2',
+
+    amarelo_claro: '#FFF97E',
+    amarelo: '#FFD954',
+    amarelo_escuro: '#FFBC00',
+
+    azul_claro: '#B0E0E6',
+    azul: '#7ABAD4',
+    azul_escuro: '#5E8FA3',
+
+    vermelho_claro: '#F4796B',
+    vermelho: '#F44E3F',
+    vermelho_escuro: '#F40000',
+  };
---
+++ src/styles/fonts.ts
@@ -0,0 +1,54 @@
+export default {
+    heading: 'SourceSansPro_600SemiBold',
+    text: 'SourceSansPro_400Regular',
+    complement: 'SourceSansPro_300Light'
+};
+
+/*Especificações para o const styles:
+
+headingG: {
+    FontFamily: fonts.heading,
+    FontSize: 20,
+},
+
+headingM: {
+    FontFamily: fonts.heading,
+    FontSize: 16,
+},
+
+headingP: {
+    FontFamily: fonts.heading,
+    FontSize: 12,
+},
+
+textG: {
+    FontFamily: fonts.text,
+    FontSize: 20,
+},
+
+textM: {
+    FontFamily: fonts.text,
+    FontSize: 16,
+},
+
+textP: {
+    FontFamily: fonts.text,
+    FontSize: 12,
+},
+
+complementM: {
+    FontFamily: fonts.complement,
+    FontSize: 20,
+},
+
+complementM: {
+    FontFamily: fonts.complement,
+    FontSize: 16,
+},
+
+complementP: {
+    FontFamily: fonts.complement,
+    FontSize: 12,
+},
+
+*/
