7e1b28aeb6810a4ae55aef224fe87ae20d954837
augustocreppe <augusto.creppe@gmail.com>
2021-07-24 10:23:07 -0300

    (exemplo de mudança)

--- src/components/TabMenu.tsx
+++ src/components/TabMenu.tsx
@@ -61,7 +61,7 @@ export default function TabMenu () {
                         
             <TouchableOpacity style={styles.button} onPress={handleComunidade} >
                 <Feather name="users" style={styles.icon2}/>
-                <Text style={styles.text2}> Comunidade </Text>
+                <Text style={styles.text2}> Comunidada </Text>
             </TouchableOpacity>
         </View>
     )
