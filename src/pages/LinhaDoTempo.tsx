3f0a619e56f2a1b0cd3301fe9fbddaa548072723
anajuliacf <69941757+anajuliacf@users.noreply.github.com>
2021-08-27 14:12:44 -0300

    Update LinhaDoTempo.tsx

--- src/pages/LinhaDoTempo.tsx
+++ src/pages/LinhaDoTempo.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { colors } from '../styles/colors';
+import fonts from '../styles/fonts';
 import { Feather } from '@expo/vector-icons';
+const profile = require('../assets/profile.png');
 
 export function LinhaDoTempo() {
     const navigation = useNavigation();
@@ -18,9 +20,37 @@ export function LinhaDoTempo() {
                 <Feather name="arrow-left" style={styles.buttonMenuIcon}/>
             </TouchableOpacity>
 
-            <View style={styles.titleView}>
-                <Text style={styles.title}>Página Linha do Tempo</Text>
+            <View style={styles.doubleView}>
+                <View style={styles.imageView}>
+                    <Image 
+                        source={profile} 
+                        style={styles.image}
+                        resizeMode="contain"
+                    />
+                </View>
+
+                <View style={styles.nameTextView}>
+                    <Text style={styles.nameText}>Fulana da Silva</Text>
+
+                    <Text style={styles.bioText}>Estou aqui para saber mais {"\n"}
+                    sobre o câncer de mama e {"\n"}
+                    participar dos grupos.</Text>
+                </View>
+
             </View>
+
+
+            <TouchableOpacity>
+                <Text style={styles.complement}>
+                     Editar perfil
+                </Text>
+            </TouchableOpacity>
+
+            <View style={styles.placeTextView}>
+                <Feather name="map-pin" style={styles.placeIcon}/>
+                <Text style={styles.placeText}>Bauru - SP</Text>
+            </View>
+
         </View>
         </SafeAreaView>
     );
@@ -31,25 +61,79 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 20,
     },
+
     buttonMenu: {
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 15,
         marginLeft: 15,
+        marginBottom: 8,
         height: 56,
         width: 56,
     },
+
+    imageView: {
+        justifyContent: 'center',
+        marginLeft: 45,
+        marginTop: 30
+    },
+
+    image: {
+        width: 100,
+        height: 100,
+    },
+
+    nameTextView: {
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 30,
+    },
+
+    nameText: {
+        fontSize: 25,
+        fontFamily: fonts.heading,
+        color: colors.body_dark,
+    },
+
+    bioText: {
+        fontSize: 14,
+        fontFamily: fonts.text,
+        color: colors.preto
+    },
+
+    complement: {
+        textDecorationLine: 'underline',
+        color: colors.body_dark,
+        marginLeft: 178,
+        fontSize: 14
+    },
+
+    doubleView: {
+        flexDirection: 'row'
+    },
+
     buttonMenuIcon: {
         fontSize: 40,
         color: colors.body_dark,
     },
-    titleView: {
+
+    placeTextView: {
+        flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        height: 590,
+        marginTop: 55,
+        marginBottom: 30,
     },
-    title: {
-        fontSize: 30,
-        fontWeight: 'bold',
+
+    placeIcon: {
+        fontSize: 20,
+        color: colors.body_dark,
+        marginRight: 10
     },
+    placeText: {
+        fontSize: 20,
+        fontFamily: fonts.text,
+        color: colors.body_dark,
+    },
+
 });
